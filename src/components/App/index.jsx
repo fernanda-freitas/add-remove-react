71caf01a214b90080e468488e2f6f62d89244bde
Fernanda Freitas Martinez <fernanda.martinez@integer.pt>
2023-01-04 11:01:34 +0000

    added top navigation

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import List from '../List'
+import Nav from '../Nav'
 
 export default function App() {
   return (
-    <div className="App text-center">
-      <List />
+    <div className='app-content'>
+      <Nav />
+      <div className="list-wrapper">
+        <List />
+      </div>
     </div>
   );
 }
